c4d487cc1ffb4b60bf51a2e8a3499629f54c18c9
Paul Choi <paulsoonminchoi@gmail.com>
2020-11-24 22:00:04 -0800

    added hydrate function

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,13 @@
  */
 
 // You can delete this file if you're not using it
+
+import ReactDOM from 'react-dom'
+
+// this is a hack to fix missing styles on refresh in production
+// reference: https://github.com/gatsbyjs/gatsby/issues/17676#issuecomment-535796737
+export function replaceHydrateFunction() {
+  return (element, container, callback) => {
+    ReactDOM.render(element, container, callback)
+  }
+}
