781bad6ebca9fd7854475afd6deb301003969f46
Paul Choi <paulsoonminchoi@gmail.com>
2021-05-21 13:09:09 -0700

    Updated resumePage to match current resume

--- src/components/resumePage.js
+++ src/components/resumePage.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React from "react"
 import styled from "styled-components"
-import resume from '../images/resume.pdf'
+import resume from "../images/resumeUpdated.pdf"
 
 const ResumeContainer = styled.div`
   display: flex;
   justify-content: center;
-`;
-
+`
 
 const ResumePage = () => (
   <>
-  <ResumeContainer>
-    <embed src={resume} width="900px" height="1150px" />
-  </ResumeContainer>
+    <ResumeContainer>
+      <embed src={resume} width="900px" height="1150px" />
+    </ResumeContainer>
   </>
 )
 
